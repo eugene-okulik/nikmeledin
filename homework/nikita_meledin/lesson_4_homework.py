617433a33ee712b901ef483b04f9977cba04132f
Nikita Meledin <nik2000meledin@mail.ru>
2025-10-15 10:53:46 +0300

    Homework 4 corrected/added space

--- homework/nikita_meledin/lesson_4_homework.py
+++ homework/nikita_meledin/lesson_4_homework.py
@@ -11,22 +11,22 @@ my_dict = {
 
 print(my_dict)
 
-print(my_dict['tuple'][-1])                        # Вывожу последжний элемент кортежа
+print(my_dict['tuple'][-1])                         # Вывожу последжний элемент кортежа
 
-my_dict['tuple'] = (1, 2.5, True, 'Hi', 1, False)  # Добавляю элемент в конец кортежа
-my_dict['tuple'] = (1, True, 'Hi', 1, False)       # Удаляю 2-ой элемент кортежа
+my_dict['tuple'] = (1, 2.5, True, 'Hi', 1, False)   # Добавляю элемент в конец кортежа
+my_dict['tuple'] = (1, True, 'Hi', 1, False)        # Удаляю 2-ой элемент кортежа
 print(my_dict['tuple'])
 
-my_dict['list'].append('1')                        # Добавляю в конец списка еще один элемент
+my_dict['list'].append('1')                         # Добавляю в конец списка еще один элемент
 print(my_dict['list'])
 
-my_dict['dict'][('i am a tuple',)] = 'i am a dict' # Добавляю элемент с ключом ('i am a tuple',)
-del my_dict['dict']['key_5']                       # Удаляю элемент
+my_dict['dict'][('i am a tuple',)] = 'i am a dict'  # Добавляю элемент с ключом ('i am a tuple',)
+del my_dict['dict']['key_5']                        # Удаляю элемент
 print(my_dict['dict'])
 
-my_dict['set'].add(None)                           # Добавляю новый элемент в множество
+my_dict['set'].add(None)                            # Добавляю новый элемент в множество
 print(my_dict['set'])
-my_dict['set'].remove(3)                           # Удаляю элемент из множества
+my_dict['set'].remove(3)                            # Удаляю элемент из множества
 print(my_dict['set'])
 
 print(my_dict)
