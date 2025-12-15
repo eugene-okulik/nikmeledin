c21708d9ae336f4fe4e254d67b31cb00ac99a0da
Nikita Meledin <nik2000meledin@mail.ru>
2025-12-15 21:55:07 +0300

    Added homework for lesson 7 after correction

--- homework/nikita_meledin/lesson_7_homework_task_3.py
+++ homework/nikita_meledin/lesson_7_homework_task_3.py
@@ -4,12 +4,14 @@ def process_line(line):
     number = int(number_str)
     print(number + 10)
 
+
 lines = [
     "результат операции: 42",
     "результат операции: 54",
     "результат работы программы: 209",
     "результат: 2"
 ]
+
 
 for line in lines:
     process_line(line)
