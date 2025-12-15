ed865fdc84df3c7427af8583ad1fc7a5c29d8de1
Nikita Meledin <nik2000meledin@mail.ru>
2025-12-15 21:51:01 +0300

    Add homework lesson 7

---
+++ homework/nikita_meledin/lesson_7_homework_task_3.py
@@ -0,0 +1,15 @@
+def process_line(line):
+    position = line.index(':') + 2
+    number_str = line[position:]
+    number = int(number_str)
+    print(number + 10)
+
+lines = [
+    "результат операции: 42",
+    "результат операции: 54",
+    "результат работы программы: 209",
+    "результат: 2"
+]
+
+for line in lines:
+    process_line(line)
