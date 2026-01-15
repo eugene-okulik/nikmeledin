1de9c5d2950f16bf9332c33609b4059b229142ef
Nikita Meledin <nik2000meledin@mail.ru>
2026-01-15 16:55:06 +0300

    Add homework lesson 11

--- homework/nikita_meledin/lesson_11_homework.py
+++ homework/nikita_meledin/lesson_11_homework.py
@@ -11,18 +11,25 @@ class Book:
         self.text = True
 
 
-book1 = Book('451 градус по Фаренгейту', 'Рэй Брэдбери', 300, '123456', False)
-book2 = Book('Тихий Дон', 'Михаил Шолохов', 200, '654321', False)
-book3 = Book('Гордость и предубеждение', 'Джейн Остин', 452, '112233', False)
-book4 = Book('Маленький принц', 'Антуан де Сент-Экзюпери', 157, '445566', False)
-book5 = Book('Робинзон Крузо', 'Даниэль Дефо', 347, '778899', True)
+book1 = Book('451 градус по Фаренгейту', 'Рэй Брэдбери',
+             300, '123456', False)
+book2 = Book('Тихий Дон', 'Михаил Шолохов',
+             200, '654321', False)
+book3 = Book('Гордость и предубеждение', 'Джейн Остин',
+             452, '112233', False)
+book4 = Book('Маленький принц', 'Антуан де Сент-Экзюпери',
+             157, '445566', False)
+book5 = Book('Робинзон Крузо', 'Даниэль Дефо',
+             347, '778899', True)
 
 
 def print_book(book):
     if book.reserved:
-        return (f"Название: {book.book_title}, Автор: {book.author}, страниц: {book.num_of_pages}, материал: {book.page_material}, зарезервирована")
+        return (f"Название: {book.book_title}, Автор: {book.author}, "
+                f"страниц: {book.num_of_pages}, материал: {book.page_material}, зарезервирована")
     else:
-        return (f"Название: {book.book_title}, Автор: {book.author}, страниц: {book.num_of_pages}, материал: {book.page_material}")
+        return (f"Название: {book.book_title}, Автор: {book.author}, "
+                f"страниц: {book.num_of_pages}, материал: {book.page_material}")
 
 
 print(print_book(book1))
@@ -31,7 +38,8 @@ print(print_book(book5))
 
 class Textbooks(Book):
     """Описание школьных учебников"""
-    def __init__(self, book_title, author, num_of_pages, ISBN, reserved, subject, school_class, availability_of_assignments):
+    def __init__(self, book_title, author, num_of_pages, ISBN, reserved,
+                 subject, school_class, availability_of_assignments):
         """Свойства учебника"""
         super().__init__(book_title, author, num_of_pages, ISBN, reserved)
         self.subject = subject
@@ -39,15 +47,19 @@ class Textbooks(Book):
         self.availability_of_assignments = availability_of_assignments
 
 
-textbook1 = Textbooks('Алгебра', 'Иванов', 200, '999888', False, 'Математика', 9, True)
-textbook2 = Textbooks('История России', 'Петров', 150, '777666', True, 'История', 8, False)
+textbook1 = Textbooks('Алгебра', 'Иванов', 200, '999888',
+                      False, 'Математика', 9, True)
+textbook2 = Textbooks('История России', 'Петров', 150, '777666',
+                      True, 'История', 8, False)
 
 
 def print_textbook(book):
     if book.reserved:
-        return (f"Название: {book.book_title}, Автор: {book.author}, страниц: {book.num_of_pages}, предмет: {book.subject}, класс: {book.school_class}, зарезервирован")
+        return (f"Название: {book.book_title}, Автор: {book.author}, страниц: {book.num_of_pages},"
+                f" предмет: {book.subject}, класс: {book.school_class}, зарезервирован")
     else:
-        return (f"Название: {book.book_title}, Автор: {book.author}, страниц: {book.num_of_pages}, предмет: {book.subject}, класс: {book.school_class}")
+        return (f"Название: {book.book_title}, Автор: {book.author}, страниц: {book.num_of_pages},"
+                f" предмет: {book.subject}, класс: {book.school_class}")
 
 
 print(print_textbook(textbook1))
