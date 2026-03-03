23613462445b93db12b5a1be975711dc67298352
Nikita Meledin <nik2000meledin@mail.ru>
2026-03-03 14:27:49 +0300

    Add homework 19

--- homework/nikita_meledin/lesson_19_homework.py
+++ homework/nikita_meledin/lesson_19_homework.py
@@ -82,10 +82,9 @@ def delete_object():
     get_response = requests.get(f"http://objapi.course.qa-practice.com/object/{object_id}")
     assert get_response.status_code == 404, 'Object should not exist'
 
-
-    get_all_object()
-    get_object_id()
-    post_object()
-    put_object()
-    patch_object()
-    delete_object()
+get_all_object()
+get_object_id()
+post_object()
+put_object()
+patch_object()
+delete_object()
